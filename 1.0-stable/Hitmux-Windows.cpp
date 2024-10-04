afc3621d2ab63458c40d237a62df266acee9c964
Caokai674674 <caokai674@gmail.com>
2024-10-04 10:24:13 +0800

    Update Hitmux-Windows.cpp

--- 1.0-stable/Hitmux-Windows.cpp
+++ 1.0-stable/Hitmux-Windows.cpp
@@ -13,7 +13,7 @@
 
 using namespace std;
 
-string Hit_PASSWD="597486",User_PASSWD="123456",USR;
+string Hit_PASSWD="654321",User_PASSWD="123456",USR;
 
 void SetConsoleColor(int color){
 	HANDLE hConsole=GetStdHandle(STD_OUTPUT_HANDLE);
