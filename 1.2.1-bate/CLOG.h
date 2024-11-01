6ea0c0f5bc6ddf59055bc9896fa8dc786ebc83fc
Hitmux <caokai674@gmail.com>
2024-11-01 20:40:06 +0800

    Update CLOG.h

--- 1.2.1-bate/CLOG.h
+++ 1.2.1-bate/CLOG.h
@@ -8,10 +8,9 @@ using namespace std;
 
 inline void clog_h(){
 	cout<<"*****************************************************\n";
-	cout<<"Hitmux 1.2.0-bate (C) 2024 CaoKai,All right reserved.\n";
-	cout<<"1. Put a large number of functions into header files to reduce development difficulty.\n";
-	cout<<"2. Added \"clog\" command.\n";
-	cout<<"3. Fix some problems of \"cla\" calculator.\n";
-	cout<<"4. Modify some UI.\n";
+	cout<<"Hitmux 1.2.1-bate (C) 2024 CaoKai,All right reserved.\n";
+	cout<<"1. Add game functions and use the command "gmh" to start.\n";
+	cout<<"2. Improve prompts, warnings, and error Us.\n";
+	cout<<"3. Fix some bugs.\n";
 }
 #endif
