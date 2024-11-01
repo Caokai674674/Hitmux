c952c76735029f74545976fcb50a0e828f284ab0
Hitmux <caokai674@gmail.com>
2024-11-01 20:34:33 +0800

    Add files via upload

---
+++ 1.2.1-bate/CLOG.h
@@ -0,0 +1,17 @@
+#ifndef CLOG_H
+#define CLOG_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+inline void clog_h(){
+	cout<<"*****************************************************\n";
+	cout<<"Hitmux 1.2.0-bate (C) 2024 CaoKai,All right reserved.\n";
+	cout<<"1. Put a large number of functions into header files to reduce development difficulty.\n";
+	cout<<"2. Added \"clog\" command.\n";
+	cout<<"3. Fix some problems of \"cla\" calculator.\n";
+	cout<<"4. Modify some UI.\n";
+}
+#endif
