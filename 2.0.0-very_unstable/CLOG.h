305b9d99018f2b4fbc8636ee21c10db91a5e1049
Hitmux <caokai674@gmail.com>
2024-11-20 22:13:59 +0800

    Add files via upload

---
+++ 2.0.0-very_unstable/CLOG.h
@@ -0,0 +1,48 @@
+#ifndef CLOG_H
+#define CLOG_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+inline void clog_h(){
+	cout<<"*****************************************************\n\n";
+	
+	cout<<"Hitmux 1.0-stable (C) 2024 CaoKai,All right reserved.\n";
+	cout<<"Hitmux is a command-line software that I developed during my free time.\n";
+	cout<<"It currently has versions for both Linux and Windows. The software is written in C++.\n";
+	cout<<"If you have any questions, you can contact me at caokai674@gmail.com.\n";
+	cout<<"*****************************************************\n\n";
+	
+	cout<<"Hitmux 1.1.0-stable (C) 2024 CaoKai,All right reserved.\n";
+	cout<<"The author lost the source code.\n";
+	cout<<"*****************************************************\n\n";
+	
+	cout<<"Hitmux 1.2.0-beta (C) 2024 CaoKai,All right reserved.\n";
+	cout<<"1. Put a large number of functions into header files to reduce development difficulty.\n";
+	cout<<"2. Added \"clog\" command.\n";
+	cout<<"3. Fix some problems of \"cla\" calculator.\n";
+	cout<<"4. Modify some UI.\n";
+	cout<<"*****************************************************\n\n";
+	
+	cout<<"Hitmux 1.2.1-beta (C) 2024 CaoKai,All right reserved.\n";
+	cout<<"1. Add game functions and use the command \"gmh\" to start.\n";
+	cout<<"2. Improve prompts, warnings, and error Us.\n";
+	cout<<"3. Fix some bugs.\n";
+	cout<<"*****************************************************\n\n";
+	
+	cout<<"Hitmux 1.2.2 (C) 2024 CaoKai,All right reserved.\n";
+	cout<<"1. Add x86 emulator\n";
+	cout<<"2. Fix startup bug\n";
+	cout<<"3. Fix some bugs.\n";
+	cout<<"*****************************************************\n";
+	
+	cout<<"Hitmux 2.0.0-very_unstable (C) 2024 CaoKai,All right reserved.\n";
+	cout<<"1.Can retain operating system data instead of losing all data every time it is shut down\n";
+	cout<<"2.Add header file \"SYSTEM.h\".\n";
+	cout<<"3.This version is very unstable, and sometimes it even fails to start properly!\n";
+	cout<<"*****************************************************\n";
+}
+
+#endif
